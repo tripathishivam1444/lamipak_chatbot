78cdec27693923c65af46ce878d5316fb6ada5b3
Utkarsh Tripathi <66895751+tripathishivam1444@users.noreply.github.com>
2024-08-27 02:14:01 +0530

    Update and rename utils_2.py to utils.py

--- utils.py
+++ utils.py
@@ -1,34 +1,29 @@
-from langchain.prompts import PromptTemplate
-from langchain.document_loaders import WebBaseLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain_community.document_loaders import SeleniumURLLoader
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.schema.output_parser import StrOutputParser
-from langchain_community.vectorstores.chroma import  Chroma 
-import streamlit as st
-from langchain_community.document_loaders import UnstructuredURLLoader
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.chat_models import ChatOpenAI
-from langchain_community.llms import OpenAI
-from dotenv import load_dotenv
-import time 
-from langchain_community.vectorstores import Qdrant
-from langchain.vectorstores import Qdrant
-from langchain.chains import RetrievalQA
-
-from langchain_openai import OpenAIEmbeddings
+import os 
+import time
 import qdrant_client
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain_openai import OpenAI
+from langchain_qdrant import Qdrant
 from langchain.chains import RetrievalQA
-from langchain_community.vectorstores import Qdrant
-from langchain_community.llms import OpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain.document_loaders import WebBaseLoader
+# from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import SeleniumURLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredURLLoader
 
+
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
 load_dotenv()
 
-
-import os 
-
-
+# Access the environment variables
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+QDERANT_URL = os.getenv('QDERANT_URL')
+QDRANT_API_API = os.getenv('QDRANT_API_API')
 
 #------------------------------------------------------------------------
 """LOad All the Pdf Data """
@@ -44,26 +39,22 @@ def pdf_to_text(pdf_files):
             all_pdf_text_list.append(text)
             source_list.append(file.name + "_page_" + str(page_number))
             
-    text_splitter = RecursiveCharacterTextSplitter( chunk_size = 1000, chunk_overlap = 20, length_function = len,)
+    text_splitter = RecursiveCharacterTextSplitter( chunk_size = 1000, chunk_overlap = 100, length_function = len,)
     
     # vector_db = Chroma.from_texts(all_pdf_text_list, OpenAIEmbeddings())
     pdf_docs = text_splitter.create_documents(all_pdf_text_list, metadatas = [{"sounrce" : s} for s in source_list])
     
     st.write( "pdf_docs -------> ",pdf_docs)
     
-    url="https://f881097e-33f9-43d6-a53f-c9d330c43384.europe-west3-0.gcp.cloud.qdrant.io:6333"
-    api_key="iYrvlwR9ksNus0h7O7-IUSyxwgJTCcGoZjgmb5pO0JkNkbRgspMQJg"
+
    
        
     qdrant = Qdrant.from_documents(pdf_docs,
                                    OpenAIEmbeddings(), 
-                                   url=url,
+                                   url=QDERANT_URL,
                                    prefer_grpc=True,
-                                   api_key=api_key,
+                                   api_key=QDRANT_API_API,
                                    collection_name="Lamipak_chatbot")
-    
-    # pdf_chroma_db = Chroma.from_documents(pdf_docs, embedding=embeddings, persist_directory= "Vector_DB/")
-    # pdf_chroma_db.persist()
     return qdrant
     
 
@@ -86,70 +77,44 @@ def web_data_loader(urls):
             web_data = UnstructuredURLLoader(urls)
             web_text = web_data.load()
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20, length_function=len)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100, length_function=len)
 
     web_docs = text_splitter.split_documents(web_text)
     st.write("web_docs -------------> ", web_docs)
 
-    # Assuming doc_content is the actual content of the document
-    embeddings = OpenAIEmbeddings()
-
-    # Creating a dummy class with page_content and metadata attributes
     class Document:
         def __init__(self, page_content, metadata):
             self.page_content = page_content
             self.metadata = metadata 
 
-    # Iterate over each document and create Chroma database
-    
     st.write(f"\n\n total ---> {len(web_docs)} documents are these \n\n ")
     st.write("\n\n Document Saving in DB ......\n\n " )
+
     for i, doc_content in enumerate(web_docs):
-        # Creating a Document instance for each document
         document = Document(doc_content.page_content, doc_content.metadata)
         print(document)
-
-        # Creating Chroma database from a single document
-        #web_chroma_db = Chroma.from_documents([document], embedding=embeddings, persist_directory="Vector_DB/")
         
-        
-        url="https://2443ceb5-760b-48d0-a96c-9c9a65b2b8f1.us-east4-0.gcp.cloud.qdrant.io:6333"
-        api_key="HX0t_FWV5dP4HH9PtI755AnehSgRmIQyLaqoQ8sCxZKJw33eUV5hWQ"
-   
-   
         qdrant = Qdrant.from_documents([document],
                                OpenAIEmbeddings(),
-                               url=url,
+                               url=QDERANT_URL,
                                prefer_grpc=True,
-                               api_key=api_key,
+                               api_key=QDRANT_API_API,
                                collection_name="Lamipak_chatbot")
         
-        st.write( f"{i}/{len(web_docs)}" )
+        st.write( f"{i+1}/{len(web_docs)}" )
         time.sleep(25)
     return qdrant
 
 
-
-
-
-url="https://2443ceb5-760b-48d0-a96c-9c9a65b2b8f1.us-east4-0.gcp.cloud.qdrant.io:6333"
-api_key="HX0t_FWV5dP4HH9PtI755AnehSgRmIQyLaqoQ8sCxZKJw33eUV5hWQ"
-    
-
-client = qdrant_client.QdrantClient(url=url, prefer_grpc=True, api_key=api_key)
+client = qdrant_client.QdrantClient(url=QDERANT_URL, prefer_grpc=True, api_key=QDRANT_API_API)
     
 vectorstore = Qdrant(client=client,
                      collection_name= 'Lamipak_chatbot',
-                     embeddings= OpenAIEmbeddings())
-
+                     embeddings= OpenAIEmbeddings(),
+                     )
 
 
 def genrating_answer_from_db(query, vectorstore= vectorstore):
-    # chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0,model_name='gpt-3.5-turbo'),
-    #                                           retriever=vectorstore.as_retriever(),
-    #                                           )
-    
-
     qa = RetrievalQA.from_chain_type(llm=OpenAI(),
                                 chain_type="stuff",
                                 retriever=vectorstore.as_retriever() )
@@ -157,16 +122,3 @@ def genrating_answer_from_db(query, vectorstore= vectorstore):
     answer = qa.run(query)
     
     return answer
-
-
-
-
-
-
-
-# print("\n\n qa ----->>  ", qa)
-# print(f"\n\n\n ------> {qa.run("what milk production in india?")}")
-    
-
-
-    
